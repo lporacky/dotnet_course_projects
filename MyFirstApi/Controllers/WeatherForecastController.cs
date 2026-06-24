7c7ae8ddcdbbf98e48369844c5d0d8a6e2208aac
Libor Poracký <plibor3@gmail.com>
2026-06-24 15:08:45 +0200

    Initial commit

---
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace MyFirstApi.Controllers;
+
+public class WeatherForecast
+
+{
+
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+}
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    // Method implementations go here
+    
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var rng = new Random();
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = rng.Next(-20, 55),
+            Summary = Summaries[rng.Next(Summaries.Length)]
+        }).ToArray();
+    }
+}
+
---
+++ SwaggerClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using CustomNamespace;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+        app.MapControllers();
+        Task.Run(() => app.RunAsync());
+//         await Task.Delay(3000);
+//         await new ClientGenerator().GenerateClient();
+        // app.Run();
+        var httpClient = new HttpClient();
+        var client = new CustomApiClient("http://localhost:5000", httpClient);
+        var user = await client.UserAsync(1);
+        Console.WriteLine($"Fetched User: {user}");
+
+    }
+}
