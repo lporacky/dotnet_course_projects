1b3a2d1940edf82e68baabede00186cc087778fd
Libor Poracký <plibor3@gmail.com>
2026-06-24 15:10:11 +0200

    Add another projects

---
+++ JsonExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Deserialize JSON to Person object
+        string json = "{\"Name\": \"John Doe\", \"Age\": 30}";
+        Person person = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+        // Serialize Person object to JSON
+        Person newPerson = new Person { Name = "Ping Jeong", Age = 25 };
+        string newJson = JsonConvert.SerializeObject(newPerson);
+        Console.WriteLine($"Serialized JSON: {newJson}");
+    }
+}
